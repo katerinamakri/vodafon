8531c32f571b0623e517506240827238bdff0042
katerinamakri <kathrin023@gmail.com>
2019-11-06 22:11:56 +0200

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Search from './Search.js';
+import Home from './Home.js';
+import Page2 from './Page2.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    
+    return (
+      <Router>
+        <div className="App">
+
+          <header>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/page2">Page 2</Link>
+                </li>
+              </ul>
+              <Search />
+            </div>
+            <div>
+              <h1>Slide 1</h1>
+              <p>Text for slide 1</p>
+            </div>
+          </header>
+
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/page2">
+              <Page2 />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );   
+  }
 }
 
 export default App;
